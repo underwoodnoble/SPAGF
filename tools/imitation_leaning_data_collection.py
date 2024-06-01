969fabfd2dc58d41e0479a29970aa0c6dc3a7623
Nobel <noble.520cn@gmail.com>
2024-06-01 17:06:02 +0800

    update

--- tools/imitation_leaning_data_collection.py
+++ tools/imitation_leaning_data_collection.py
@@ -1,25 +1,60 @@
 import os
+import argparse
 from typing import List
 
 from dotenv import load_dotenv
 from openai import OpenAI
 
 
-def get_response(messages: List):
-    client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"),
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
-    )
-    
-    completion = client.chat.completions.create(
-        model="qwen-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "你是谁？"}
-        ]
-    )
-    print(completion.model_dump_json())
-    
-    
+def set_api(model, temperature):
+    def get_response(messages: List) -> str:
+        client = OpenAI(
+            api_key=os.getenv("DASHSCOPE_API_KEY"),
+            base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
+        )
+        
+        resp = client.chat.completions.create(
+            model=model,
+            messages=messages,
+            temperature=temperature
+        )
+        
+        return resp.to_dict()['choices'][0]['message']['content']
+    return get_response
+
+
+def main(args):
+    print(get_response([{"role": "user", "content": "请问你是谁？"}]))
+
+
 if __name__ == '__main__':
-    get_response()
+    load_dotenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model',
+        type=str,
+        default='qwen-turbo',
+        choices=['qwen-turbo']
+    )
+    parser.add_argument(
+        '--concept_path',
+        type=str
+    )
+    parser.add_argument(
+        '--save_path',
+        type=str
+    )
+    parser.add_argument(
+        '--temperature',
+        type=float,
+        default=1.0
+    )
+    parser.add_argument(
+        '--max_concept_length',
+        type=int,
+        default=3,
+        help="Maximum number of words in a concept."
+    )
+    args = parser.parse_args()
+    get_response = set_api(args.model, args.temperature)
+    main(args)
